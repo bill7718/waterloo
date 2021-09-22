5ebf7cbc95009009bca1c85b1646f3f78a23d20b
stoke <stokes7718@gmail.com>
2021-09-22 17:29:35 +0100

    hello

---
+++ lib/src/data_object_switch_tile.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:serializable_data/serializable_data.dart';
+
+import 'waterloo_switch_tile.dart';
+
+class DataObjectSwitchTile extends StatelessWidget {
+  /// The label for the field
+  final String label;
+
+  final DataObject data;
+
+  ///
+  final String fieldName;
+
+  const DataObjectSwitchTile({Key? key,
+    required this.label,
+    required this.data,
+    required this.fieldName
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WaterlooSwitchTile(
+      label: label,
+      initialValue: data.get(fieldName) ?? false,
+      valueBinder: (v) { data.set(fieldName, v); },
+    );
+  }
+}
