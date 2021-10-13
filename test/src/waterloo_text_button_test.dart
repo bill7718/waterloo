6604c326ffa2143dd7ef7b51bbb22769abefa9b2
stoke <stokes7718@gmail.com>
2021-10-13 23:32:44 +0100

    hello

--- test/src/waterloo_text_button_test.dart
+++ test/src/waterloo_text_button_test.dart
@@ -15,62 +15,62 @@ void main() {
     exceptionThrown = true;
   }
 
-  setUp(() {
-    pressed = false;
-    exceptionThrown = false;
-  });
+  group('Test WaterlooTextButton', ()
+  {
+    setUp(() {
+      pressed = false;
+      exceptionThrown = false;
+    });
 
- testWidgets('Add a WaterlooTextButton to a page', (WidgetTester tester) async {
+    testWidgets('Add a WaterlooTextButton to a page', (WidgetTester tester) async {
+      var text = 'Hello';
+      var expectedButtonText = MockTextProvider.text(text) ?? '';
 
-   var text = 'Hello';
-   var expectedButtonText = MockTextProvider.text(text) ?? '';
-
-    Widget page = MockPage(
-      WaterlooTextButton(text: text, exceptionHandler: exceptionHandler ,
-        onPressed: () {
-        pressed = true;
-        },
+      Widget page = MockPage(
+          WaterlooTextButton(text: text, exceptionHandler: exceptionHandler,
+            onPressed: () {
+              pressed = true;
+            },
 
           )
-    );
+      );
 
-    await tester.pumpWidget(page);
-    expect(find.text(expectedButtonText), findsOneWidget);
-    var f = findButtonByText(text);
-    expect (f, findsOneWidget);
-  });
+      await tester.pumpWidget(page);
+      expect(find.text(expectedButtonText), findsOneWidget);
+      var f = findButtonByText(text);
+      expect(f, findsOneWidget);
+    });
 
-  testWidgets('Tap the button', (WidgetTester tester) async {
-    Widget page = MockPage(
-      WaterlooTextButton(text: 'Hello', exceptionHandler: exceptionHandler ,
-        onPressed: () {
-          pressed = true;
-        },
+    testWidgets('Tap the button', (WidgetTester tester) async {
+      Widget page = MockPage(
+          WaterlooTextButton(text: 'Hello', exceptionHandler: exceptionHandler,
+            onPressed: () {
+              pressed = true;
+            },
 
-      )
-    );
+          )
+      );
 
-    await tester.pumpWidget(page);
-    await tap('Hello',  tester);
-    expect(pressed, true);
+      await tester.pumpWidget(page);
+      await tap('Hello', tester);
+      expect(pressed, true);
+    });
 
-  });
 
+    testWidgets('Tap the button but generate an exception', (WidgetTester tester) async {
+      Widget page = MockPage(
+          WaterlooTextButton(text: 'Hello', exceptionHandler: exceptionHandler,
+            onPressed: () {
+              throw Exception('hi');
+            },
 
-  testWidgets('Tap the button but generate an exception', (WidgetTester tester) async {
-    Widget page = MockPage(
-      WaterlooTextButton(text: 'Hello', exceptionHandler: exceptionHandler ,
-        onPressed: () {
-          throw Exception('hi');
-        },
+          )
+      );
 
-      )
-    );
-
-    await tester.pumpWidget(page);
-    await tap('Hello',  tester);
-    expect(exceptionThrown, true);
-
+      await tester.pumpWidget(page);
+      await tap('Hello', tester);
+      expect(exceptionThrown, true);
+    });
   });
 }
 
