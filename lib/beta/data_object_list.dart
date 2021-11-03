b6fc9499e19148d2aae0f24f177a5e6f9e895b2d
stoke <stokes7718@gmail.com>
2021-11-03 16:38:42 +0000

    hello

--- lib/beta/data_object_list.dart
+++ lib/beta/data_object_list.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:serializable_data/serializable_data.dart';
-import '../src/waterloo_drop_down_list.dart';
 import '../src/waterloo_future_drop_down_list.dart';
+import 'data_list_getter.dart';
 
 ///
 /// Shows data from a [DataObject] as a list. Wraps a [Future] around a [WaterlooFutureDropDownList]
@@ -81,64 +81,3 @@ class DataObjectList<T extends PersistableDataObject> extends StatelessWidget {
   }
 }
 
-///
-/// Gets the data from the database used by a [DataObjectList]
-///
-class DataListGetter<T extends PersistableDataObject> implements ListGetter {
-
-  /// The field used to filter the data.
-  ///
-  ///
-  /// {@macro filter}
-  final String? filterLabel;
-
-  /// The object type to use in the filter. If not supplied then this value is derived from the
-  /// parameter T
-  ///
-  ///
-  /// {@macro filter}
-  final String? filterRef;
-
-  /// The value to use for the filter
-  ///
-  ///
-  /// {@macro filter}
-  final dynamic filterValue;
-
-  /// This label identifies the value to show on the screen that is selected
-  ///
-  /// {@macro filter}
-  final String descriptionLabel;
-
-  /// The label of value actually selected (normally an id)
-  ///
-  ///
-  /// {@macro filter}
-  final String idLabel;
-
-  /// The object used to retrieve the data from the database
-  final DatabaseReader reader;
-
-  DataListGetter(this.filterLabel, this.filterValue,
-    this.descriptionLabel, this.idLabel, this.filterRef, this.reader);
-
-  @override
-  Future<List<ListItem>> getList() {
-    var c = Completer<List<ListItem>>();
-
-    var f = reader.query<T>(ref: filterRef, field: filterLabel, value: filterValue);
-
-    f.then( (r) {
-      var response = <ListItem>[];
-      for (var item in r) {
-        response.add(ListItem(item.get(idLabel),  item.get(descriptionLabel)));
-      }
-      c.complete(response);
-
-    });
-
-    return c.future;
-  }
-
-
-}
---
+++ test/beta/data_list_getter_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:waterloo/waterloo.dart';
+
+void main() {
+
+
+  group('Test DataListGetter', () {
+    setUp(() {
+
+    });
+
+    testWidgets(
+        'Create a change notifier and amend it checking that notifications are triggered', (WidgetTester tester) async {
+
+    });
+  });
+}
